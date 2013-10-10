e1848556139fe73a6f8bbe566fe672445871e7e4
slash6475 <slash6475@slash-computer.staff.ece.fr>
2013-10-10 14:31:44 +0200

    Fix some echo

--- install.sh
+++ install.sh
@@ -16,18 +16,14 @@ else
 	DEST="$1"
 fi
 
-echo "Installing 'emma-node' ..."
-
-echo -e  "=========================="
-echo -e  "  Contiki installation"
-echo -e  "=========================="
+echo -e  "Contiki installation"
+echo -e  "===================="
 cp -R $THIS/dep/* $DEST/
 echo -e ""
 
 # Patch Contiki OS
-echo -e  "==================="
-echo -e  "  Contiki patching"
-echo -e  "==================="
+echo -e  "Contiki patching"
+echo -e  "================"
 patch -p2 --directory=$DEST --input=$THIS/patch/patch-slip-raven.diff
 patch -p2 --directory=$DEST --input=$THIS/patch/patch_erbium.diff
 patch -p2 --directory=$DEST --input=$THIS/patch/raven-main_patch.diff
@@ -39,26 +35,23 @@ patch -p2 --directory=$DEST --input=$THIS/patch/cfs_patch.diff
 echo -e ""
 
 # Copy EMMA application and example
-echo -e  "=========================="
-echo -e  "  emma-node installation"
-echo -e  "=========================="
+echo -e  "emma-node installation"
+echo -e  "======================"
 cp -r $THIS/src/apps/emma-node $DEST/apps/
 cp -r $THIS/src/examples/emma-node-example $DEST/examples/
 cp $THIS/network.sh $DEST/../
 echo -e ""
 
 # Copy EMMA documentation
-echo -e  "=========================="
-echo -e  "     doc installation"
-echo -e  "=========================="
+echo -e  "doc installation"
+echo -e  "================"
 cp -r $THIS/doc $DEST/../
 echo -e ""
 
 # Install compilator toolchain
-echo -e  "=========================="
-echo -e  "   Package installation"
-echo -e  "=========================="
+echo -e  "Package installation"
+echo -e  "===================="
 sudo apt-get install gcc-avr avr-libc avrdude
 echo -e ""
 
-echo "Done."
+echo     "Done."
